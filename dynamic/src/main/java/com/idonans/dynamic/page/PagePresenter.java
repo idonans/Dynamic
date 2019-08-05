5b821a77c0749f2eee5eed36e5c420fbdced25e6
idonans <idonans@126.com>
2019-08-05 15:10:40 +0800

    add page view & page presenter

---
+++ dynamic/src/main/java/com/idonans/dynamic/page/PagePresenter.java
@@ -0,0 +1,203 @@
+package com.idonans.dynamic.page;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.WorkerThread;
+
+import com.idonans.dynamic.DynamicPresenter;
+import com.idonans.lang.DisposableHolder;
+
+import java.util.Collection;
+import java.util.Objects;
+
+import io.reactivex.Single;
+import io.reactivex.SingleSource;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
+import timber.log.Timber;
+
+public abstract class PagePresenter<E, T extends PageView<E>> extends DynamicPresenter<T> {
+
+    protected final DisposableHolder mInitRequestHolder = new DisposableHolder();
+    protected final DisposableHolder mPrePageRequestHolder = new DisposableHolder();
+    protected final DisposableHolder mNextPageRequestHolder = new DisposableHolder();
+    private final DisposableHolder[] mRequestHolders = {mInitRequestHolder, mPrePageRequestHolder, mNextPageRequestHolder};
+
+    public PagePresenter(T view) {
+        super(view);
+    }
+
+    /**
+     * 除了指定的请求外，清除其它请求。
+     *
+     * @param excepts
+     */
+    public void clearRequestExcept(DisposableHolder... excepts) {
+        for (DisposableHolder target : mRequestHolders) {
+            boolean matchExcept = false;
+            if (excepts != null) {
+                for (DisposableHolder except : excepts) {
+                    if (target == except) {
+                        matchExcept = true;
+                        break;
+                    }
+                }
+            }
+            if (!matchExcept) {
+                target.clear();
+            }
+        }
+    }
+
+    /**
+     * 请求初始数据
+     */
+    public void requestInit() {
+        // 请求初始数据时，中断所有的旧请求
+        clearRequestExcept();
+
+        PageView<E> view = getView();
+        if (view == null) {
+            Timber.e("view is null");
+            return;
+        }
+        view.showInitLoading();
+
+        mInitRequestHolder.set(Single.just(this)
+                .flatMap((Function<Object, SingleSource<Collection<E>>>) object -> createInitRequest())
+                .map(Objects::requireNonNull)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(items -> {
+                    PageView<E> innerView = getView();
+                    if (innerView == null) {
+                        Timber.e("view is null");
+                        return;
+                    }
+                    innerView.hideInitLoading();
+                    onInitRequestResult(innerView, items);
+                }, e -> {
+                    PageView<E> innerView = getView();
+                    if (innerView == null) {
+                        Timber.e("view is null");
+                        return;
+                    }
+                    innerView.hideInitLoading();
+                    onInitRequestError(innerView, e);
+                }));
+    }
+
+    /**
+     * 请求上一页数据
+     */
+    public void requestPrePage() {
+        // 请求上一页数据时，可以与下一页请求同时存在
+        clearRequestExcept(mNextPageRequestHolder);
+
+        PageView<E> view = getView();
+        if (view == null) {
+            Timber.e("view is null");
+            return;
+        }
+        view.showPrePageLoading();
+
+        mInitRequestHolder.set(Single.just(this)
+                .flatMap((Function<Object, SingleSource<Collection<E>>>) object -> createPrePageRequest())
+                .map(Objects::requireNonNull)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(items -> {
+                    PageView<E> innerView = getView();
+                    if (innerView == null) {
+                        Timber.e("view is null");
+                        return;
+                    }
+                    innerView.hidePrePageLoading();
+                    onPrePageRequestResult(innerView, items);
+                }, e -> {
+                    PageView<E> innerView = getView();
+                    if (innerView == null) {
+                        Timber.e("view is null");
+                        return;
+                    }
+                    innerView.hidePrePageLoading();
+                    onPrePageRequestError(innerView, e);
+                }));
+    }
+
+    /**
+     * 请求下一页数据
+     */
+    public void requestNextPage() {
+        // 请求下一页数据时，可以与上一页请求同时存在
+        clearRequestExcept(mPrePageRequestHolder);
+
+        PageView<E> view = getView();
+        if (view == null) {
+            Timber.e("view is null");
+            return;
+        }
+        view.showNextPageLoading();
+
+        mInitRequestHolder.set(Single.just(this)
+                .flatMap((Function<Object, SingleSource<Collection<E>>>) object -> createNextPageRequest())
+                .map(Objects::requireNonNull)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(items -> {
+                    PageView<E> innerView = getView();
+                    if (innerView == null) {
+                        Timber.e("view is null");
+                        return;
+                    }
+                    innerView.hideNextPageLoading();
+                    onNextPageRequestResult(innerView, items);
+                }, e -> {
+                    PageView<E> innerView = getView();
+                    if (innerView == null) {
+                        Timber.e("view is null");
+                        return;
+                    }
+                    innerView.hideNextPageLoading();
+                    onNextPageRequestError(innerView, e);
+                }));
+    }
+
+    @WorkerThread
+    @Nullable
+    protected abstract SingleSource<Collection<E>> createInitRequest() throws Exception;
+
+    protected void onInitRequestResult(@NonNull PageView<E> view, @NonNull Collection<E> items) {
+        view.onInitDataLoad(items);
+    }
+
+    protected void onInitRequestError(@NonNull PageView<E> view, @NonNull Throwable e) {
+        view.onInitDataLoadFail(e);
+    }
+
+    @WorkerThread
+    @Nullable
+    protected abstract SingleSource<Collection<E>> createPrePageRequest() throws Exception;
+
+    protected void onPrePageRequestResult(@NonNull PageView<E> view, @NonNull Collection<E> items) {
+        view.onPrePageDataLoad(items);
+    }
+
+    protected void onPrePageRequestError(@NonNull PageView<E> view, @NonNull Throwable e) {
+        view.onPrePageDataLoadFail(e);
+    }
+
+    @WorkerThread
+    @Nullable
+    protected abstract SingleSource<Collection<E>> createNextPageRequest() throws Exception;
+
+    protected void onNextPageRequestResult(@NonNull PageView<E> view, @NonNull Collection<E> items) {
+        view.onNextPageDataLoad(items);
+    }
+
+    protected void onNextPageRequestError(@NonNull PageView<E> view, @NonNull Throwable e) {
+        view.onNextPageDataLoadFail(e);
+    }
+
+}
---
+++ dynamic/src/main/java/com/idonans/dynamic/page/PageView.java
@@ -0,0 +1,33 @@
+package com.idonans.dynamic.page;
+
+import com.idonans.dynamic.DynamicView;
+
+import java.util.Collection;
+
+public interface PageView<E> extends DynamicView {
+
+    void showInitLoading();
+
+    void hideInitLoading();
+
+    void showPrePageLoading();
+
+    void hidePrePageLoading();
+
+    void showNextPageLoading();
+
+    void hideNextPageLoading();
+
+    void onInitDataLoad(Collection<E> items);
+
+    void onInitDataLoadFail(Throwable e);
+
+    void onPrePageDataLoad(Collection<E> items);
+
+    void onPrePageDataLoadFail(Throwable e);
+
+    void onNextPageDataLoad(Collection<E> items);
+
+    void onNextPageDataLoadFail(Throwable e);
+
+}
