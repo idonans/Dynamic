56c80c3b8144ac1559d320ce2895d9ecf862c3ad
idonans <idonans@126.com>
2019-08-22 13:10:13 +0800

    add UnionTypeLoadingStatus no more data

--- dynamic/src/main/java/com/idonans/dynamic/uniontype/loadingstatus/UnionTypeLoadingStatus.java
+++ dynamic/src/main/java/com/idonans/dynamic/uniontype/loadingstatus/UnionTypeLoadingStatus.java
@@ -4,6 +4,7 @@ import com.idonans.dynamic.uniontype.loadingstatus.impl.UnionTypeLoadingStatusLo
 import com.idonans.dynamic.uniontype.loadingstatus.impl.UnionTypeLoadingStatusLoadFailSmallViewHolder;
 import com.idonans.dynamic.uniontype.loadingstatus.impl.UnionTypeLoadingStatusLoadingLargeViewHolder;
 import com.idonans.dynamic.uniontype.loadingstatus.impl.UnionTypeLoadingStatusLoadingSmallViewHolder;
+import com.idonans.dynamic.uniontype.loadingstatus.impl.UnionTypeLoadingStatusNoMoreDataViewHolder;
 import com.idonans.uniontype.SimpleUnionTypeMapper;
 
 public class UnionTypeLoadingStatus extends SimpleUnionTypeMapper {
@@ -12,12 +13,14 @@ public class UnionTypeLoadingStatus extends SimpleUnionTypeMapper {
     public static final int UNION_TYPE_LOADING_STATUS_LOADING_SMALL = -1001;
     public static final int UNION_TYPE_LOADING_STATUS_LOAD_FAIL_LARGE = -1002;
     public static final int UNION_TYPE_LOADING_STATUS_LOAD_FAIL_SMALL = -1003;
+    public static final int UNION_TYPE_LOADING_STATUS_NO_MORE_DATA = -1004;
 
     public UnionTypeLoadingStatus() {
         put(UNION_TYPE_LOADING_STATUS_LOADING_LARGE, UnionTypeLoadingStatusLoadingLargeViewHolder::new);
         put(UNION_TYPE_LOADING_STATUS_LOADING_SMALL, UnionTypeLoadingStatusLoadingSmallViewHolder::new);
         put(UNION_TYPE_LOADING_STATUS_LOAD_FAIL_LARGE, UnionTypeLoadingStatusLoadFailLargeViewHolder::new);
         put(UNION_TYPE_LOADING_STATUS_LOAD_FAIL_SMALL, UnionTypeLoadingStatusLoadFailSmallViewHolder::new);
+        put(UNION_TYPE_LOADING_STATUS_NO_MORE_DATA, UnionTypeLoadingStatusNoMoreDataViewHolder::new);
     }
 
 }
---
+++ dynamic/src/main/java/com/idonans/dynamic/uniontype/loadingstatus/impl/UnionTypeLoadingStatusNoMoreDataViewHolder.java
@@ -0,0 +1,14 @@
+package com.idonans.dynamic.uniontype.loadingstatus.impl;
+
+import androidx.annotation.NonNull;
+
+import com.idonans.dynamic.R;
+import com.idonans.uniontype.Host;
+
+public class UnionTypeLoadingStatusNoMoreDataViewHolder extends UnionTypeLoadingStatusViewHolder {
+
+    public UnionTypeLoadingStatusNoMoreDataViewHolder(@NonNull Host host) {
+        super(host, R.layout.union_type_loading_status_no_more_data_view);
+    }
+
+}
