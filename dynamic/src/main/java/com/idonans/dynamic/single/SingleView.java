100a33eb47611c6f9218c580cf8a90cba9987a12
idonans <idonans@126.com>
2020-02-06 12:24:56 +0800

    add SingleView, SinglePresenter support

---
+++ dynamic/src/main/java/com/idonans/dynamic/single/SingleView.java
@@ -0,0 +1,21 @@
+package com.idonans.dynamic.single;
+
+import androidx.annotation.NonNull;
+
+import com.idonans.dynamic.DynamicView;
+
+import java.util.Collection;
+
+public interface SingleView<E> extends DynamicView {
+
+    void showInitLoading();
+
+    void hideInitLoading();
+
+    void onInitDataLoad(@NonNull Collection<E> items);
+
+    void onInitDataEmpty();
+
+    void onInitDataLoadFail(@NonNull Throwable e);
+
+}
